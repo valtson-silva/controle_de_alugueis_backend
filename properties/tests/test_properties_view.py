ffe9867b576d76def6c7e38266731c2a94036625
Váltson Araújo <valtson88@gmail.com>
2025-02-12 22:29:48 -0300

    Projeto atualizado

--- properties/tests/test_properties_view.py
+++ properties/tests/test_properties_view.py
@@ -9,11 +9,11 @@ def test_properties_list():
     client = APIClient()
     # Registra um usuário de teste
     user = User.objects.create_user(username="testuser", password="testpass")
-    # Obtém a url
+    
     url = reverse('properties_get')
-    # Faz o login
+    
     client.login(username="testuser", password="testpass")
-    # Faz a requisição get
+    
     response = client.get(url)
     
     # Verifica se o status retornado foi 200 OK
@@ -21,15 +21,15 @@ def test_properties_list():
     
 @pytest.mark.django_db
 def test_create_tenant():
-     # Simula um cliente HTTP
+     
     client = APIClient()
-    # Registra um usuário de teste
+    
     user = User.objects.create_user(username="testuser", password="testpass")
-    # Obtém a url
+    
     url = reverse('property_create')
-    # Faz o login
+    
     client.login(username="testuser", password="testpass")
-    # Faz a requisição post
+    
     response = client.post(url, {
         "address": "Rua A",
         "type": "casa",
