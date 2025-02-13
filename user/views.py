6b30aea702dc6d81f6e0efedd86f25169d3ca315
Váltson Araújo <valtson88@gmail.com>
2025-02-12 22:45:30 -0300

    Projeto atualizado

--- user/views.py
+++ user/views.py
@@ -14,16 +14,16 @@ class UserRegisterView(APIView):
         return render(request, 'register.html')
     
     def post(self, request):
-        # Serializa os dados recebidos no corpo da requisição
+        
         serializer = UserSerializer(data=request.data)
-        # Verifica se os dados enviados são válidos
+        
         if serializer.is_valid():
             # Salva o usuário no banco de dados
             serializer.save()
-            # Retorna uma mensagem e o status 201
+            
             return Response({"message": "Registro realizado com sucesso."}, status=status.HTTP_201_CREATED)
         else:
-            # Retorna uma mensagem e o status 400
+            
             return Response({"message": "Dados informados inválidos."}, status=status.HTTP_400_BAD_REQUEST)
  
 class UserLogarView(APIView):
@@ -47,10 +47,10 @@ class UserLogarView(APIView):
         if user is not None:
             # Faz o login do usuário
             login(request, user)
-            # Retorna uma mensagem e o status 200
+            
             return Response({"message": "Login realizado com sucesso."}, status=status.HTTP_200_OK)
         else:
-            # Retorna uma mensagem e o status 401
+            
             return Response({"message": "Credenciais inválidas."}, status=status.HTTP_401_UNAUTHORIZED)
         
 class LogoutView(APIView):
@@ -62,6 +62,6 @@ class LogoutView(APIView):
     def post(self, request):
         # Faz o logout
         logout(request)
-        # Retorna uma mensagem e o status 200
+        
         return Response({"message": "Logout realizado com sucesso."}, status=status.HTTP_200_OK)
         
